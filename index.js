2650ff2ee262dd3d3e71056be1b5a8c9491aa242
brendanatme <registrations@brendanat.me>
2018-12-21 14:16:32 -0500

    Add config file support

--- index.js
+++ index.js
@@ -2,17 +2,60 @@
 
 /**
  * rg-cli
+ * 
+ * provide CLI for generating react components quickly
  */
+const fs = require('fs');
+const path = require('path');
 const argv = require('minimist')(process.argv.slice(2));
 const generateComponent = require('./src/generate-component');
 
 const [arg1, arg2, name] = argv._;
 
+console.log('-------------------');
+console.log(' ');
+console.log(' RG-CLI');
+console.log(' ');
+
 if (arg1 !== 'g' || arg2 !== 'c') {
-  console.log('Whoops! The only supported command for `rg` is currently `rg g c`, for generating components');
-  console.log('Example usage: `rg g c text-input --dir=/my/favouritest/dir/ever');
+  console.log(' Whoops! The only supported command for `rg` is currently `rg g c`, for generating components');
+  console.log(' usage: `rg g c [component-name] --dir=path/to/components --type=[js|ts]');
   
   process.exit(1);
 }
 
-generateComponent(name, argv.ts, argv.dir);
+fs.readFile(path.resolve(process.cwd(), 'rg.config.json'), (err, str) => {
+  const options = {
+    dir: path.join('src', 'components'),
+    type: 'js',
+  };
+
+  if (err) {
+    console.log(' No rg.config.json file found in CWD');
+  } else {
+    console.log(' Found rg.config.json file');
+    const config = JSON.parse(str);
+
+    if (config.dir && !argv.dir) {
+      options.dir = config.dir;
+      console.log(` Using value from rg.config.json: dir=${options.dir}`);
+    }
+
+    if (config.type && !argv.type) {
+      options.type = config.type;
+      console.log(` Using value from rg.config.json: type=${options.type}`);
+    }
+  }
+  
+  if (argv.dir) {
+    options.dir = argv.dir;
+    console.log(` Using value from CLI arg: dir=${options.dir}`);
+  }
+
+  if (argv.type) {
+    options.type = argv.type;
+    console.log(` Using value from CLI arg: type=${options.type}`);
+  }
+
+  generateComponent(name, options);
+});
--- src/generate-component.js
+++ src/generate-component.js
@@ -19,11 +19,11 @@ const { execSync } = require('child_process');
 const { ClassCase } = require('./helpers');
 const templates = require('./templates');
 
-const generateComponent = (name, isTS, dir) => {
-  const ext = isTS ? 'ts' : 'js';
+const generateComponent = (name, options) => {
+  const ext = options.type;
   const render = templates[ext];
 
-  console.log('rg-cli: Generating component...');
+  console.log(' Generating component...');
   
   if (!name) {
     console.log('Whoops! please enter a name for your component.');
@@ -32,38 +32,36 @@ const generateComponent = (name, isTS, dir) => {
     process.exit(1);
   }
   
-  const componentsDir = dir && dir.length
-    ? path.resolve(process.cwd(), dir)
-    : path.resolve(process.cwd(), 'src', 'components');
-  const componentDir = path.resolve(componentsDir, name);
+  const componentDir = path.resolve(options.dir, name);
   
-  console.log(`Making dir... ${componentDir}`);
+  console.log(` Making dir... ${componentDir}`);
   execSync(`mkdir ${componentDir}`);
   
-  console.log(`making ${name}.component.scss...`);
+  console.log(` Making ${name}.component.scss...`);
   execSync(`echo "${render.scss(name)}" >> ${componentDir}/${name}.component.scss`);
   
-  console.log(`making ${name}.component.${ext}x...`);
+  console.log(` Making ${name}.component.${ext}x...`);
   execSync(`echo "${render.component(name)}" >> ${componentDir}/${name}.component.${ext}x`);
   
-  console.log(`making ${name}.container.${ext}x...`);
+  console.log(` Making ${name}.container.${ext}x...`);
   execSync(`echo "${render.container(name)}" >> ${componentDir}/${name}.container.${ext}x`);
   
   // console.log(`making ${name}.test.${ext}x...`);
   // execSync(`echo "${render.test(name)}" >> ${componentDir}/${name}.test.${ext}x`);
   
-  console.log(`making index.${ext}...`);
+  console.log(` Making index.${ext}...`);
   execSync(`echo "${render.mod(name)}" >>  ${componentDir}/index.${ext}`);
   
-  console.log('success!');
-  console.log('include your component like so:');
-  console.log('');
+  console.log(' Success!');
+  console.log(' Include your component like so:');
+  console.log(' ');
   if (ext === 'ts') {
-    console.log(`  import { ${ClassCase(name)} } from '~/components/${name}';`);
+    console.log(`   import { ${ClassCase(name)} } from '~/components/${name}';`);
   } else {
-    console.log(`  import ${ClassCase(name)} from '~/components/${name}';`);
+    console.log(`   import ${ClassCase(name)} from '~/components/${name}';`);
   }
-  console.log('');
+  console.log(' ');
+  console.log('-------------------');
 };
 
 module.exports = generateComponent;
